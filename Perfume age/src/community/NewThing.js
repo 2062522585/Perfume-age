fdc057fb4c2cef395c967d1ed05cce1af0acfa50
3138295652 <xiao_m_j@qq.com>
2020-09-16 20:21:30 +0800

    初始化导航栏及项目结构

---
+++ Perfume age/src/community/NewThing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class School extends React.Component {
+	constructor() {
+		super();
+	}
+	 
+	render() {
+		return <div >这是新鲜事</div>
+	}
+}
---
+++ Perfume age/src/home/Leaderboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class School extends React.Component {
+	constructor() {
+		super();
+	}
+	 
+	render() {
+		return <div >这是排行榜</div>
+	}
+}
